f71ae7f3f9259b5ccb1fb916c09e4eaeb08d2cb4
vinsst <vinsstcompany@gmail.com>
2024-03-21 17:28:55 +0300

    testdfghisfsdfsghjhg

--- server/index.js
+++ server/index.js
@@ -86,7 +86,7 @@ printDirectoryStructure(projectRootPath);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(express.static(path.join(dir_rootApp, "/client/build")));
+app.use(express.static(path.join(dir_rootApp, "/client/build")));
 
 console.log(path.join(dir_rootApp, "/client/build"));
 console.log(__dirname);
